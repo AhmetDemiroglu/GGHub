0093e51f7361e6e3629598ed1533dd6566643e71
Ahmet Demiroğlu <ahmetdemiroglu89@hotmail.com>
2025-08-15 21:40:10 +0300

    .

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "../components/ui/button";
+
 export default function HomePage() {
   return (
-    <main>
-      <h1>Ana Sayfa</h1>
+    <main className="flex items-center justify-center h-screen">
+      <div>
+        <h1 className="text-2xl mb-4">Ana Sayfa</h1>
+        <Button variant="outline">Merhaba ShadCN</Button>
+      </div>
     </main>
   );
 }
