d23920cde22baa46dafbd948f8e862e919052d0e
Ahmet Demiroğlu <ahmetdemiroglu89@gmail.com>
2025-10-05 10:55:10 +0300

    ..

--- ui/src/api/profile/profile.api.ts
+++ ui/src/api/profile/profile.api.ts
@@ -1,29 +1,34 @@
-import {axiosInstance} from '@core/lib/axios';
-import { Profile, ProfileForUpdate, UpdateProfileVisibilityDto, UpdateMessageSettingDto} from '@/models/profile/profile.model';
-import { AxiosResponse } from 'axios';
+import { axiosInstance } from "@core/lib/axios";
+import { Profile, ProfileForUpdate, UpdateProfileVisibilityDto, UpdateMessageSettingDto } from "@/models/profile/profile.model";
+import { AxiosResponse } from "axios";
+import { PublicProfile } from "@/models/profile/profile.model";
 
 export const getMyProfile = async (): Promise<Profile> => {
-  const response = await axiosInstance.get<Profile>('/profile/me');
-  return response.data;
+    const response = await axiosInstance.get<Profile>("/profile/me");
+    return response.data;
 };
 export const updateMyProfile = async (data: ProfileForUpdate): Promise<Profile> => {
-  const response = await axiosInstance.put<Profile>('/profile/me', data);
-  return response.data;
+    const response = await axiosInstance.put<Profile>("/profile/me", data);
+    return response.data;
 };
 export const updateProfileVisibility = async (data: UpdateProfileVisibilityDto) => {
-  return axiosInstance.put('/profile/me/visibility', data);
+    return axiosInstance.put("/profile/me/visibility", data);
 };
 
 export const exportMyData = async (): Promise<AxiosResponse<Blob>> => {
-  return axiosInstance.get('/profile/me/export-data', {
-    responseType: 'blob',
-  });
+    return axiosInstance.get("/profile/me/export-data", {
+        responseType: "blob",
+    });
 };
 
 export const deleteMyAccount = async (): Promise<void> => {
-  await axiosInstance.delete('/profile/me');
+    await axiosInstance.delete("/profile/me");
 };
 
 export const updateMessageSetting = async (data: UpdateMessageSettingDto) => {
-  return axiosInstance.put('/profile/me/message-setting', data);
+    return axiosInstance.put("/profile/me/message-setting", data);
+};
+
+export const getProfileByUsername = (username: string): Promise<PublicProfile> => {
+    return axiosInstance.get<PublicProfile>(`/profiles/${username}`).then((response) => response.data);
 };
