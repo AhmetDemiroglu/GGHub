b9c22576b9d62ba4c400d59abab81c61ea1e9bb2
Ahmet Demiroğlu <ahmetdemiroglu89@hotmail.com>
2025-11-02 01:25:40 +0300

    +

--- backend/GGHub.Infrastructure/Services/EmailService.cs
+++ backend/GGHub.Infrastructure/Services/EmailService.cs
@@ -23,7 +23,7 @@ namespace GGHub.Infrastructure.Services
             email.Body = new TextPart(TextFormat.Html) { Text = body };
 
             using var smtp = new SmtpClient();
-            smtp.ServerCertificateValidationCallback = (s, c, h, e) => true;
+            //smtp.ServerCertificateValidationCallback = (s, c, h, e) => true;
 
             //await smtp.ConnectAsync(_config["EmailSettings:Host"], int.Parse(_config["EmailSettings:Port"]!), SecureSocketOptions.StartTls);
 
