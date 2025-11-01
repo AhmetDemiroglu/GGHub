f4c365115af2b383640d7ad6a72fed31824634aa
Ahmet Demiroğlu <ahmetdemiroglu89@hotmail.com>
2025-11-01 04:48:16 +0300

    +

--- backend/GGHub.WebAPI/Program.cs
+++ backend/GGHub.WebAPI/Program.cs
@@ -195,7 +195,7 @@ if (app.Environment.IsProduction())
 
     app.Use(async (context, next) =>
     {
-        context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
+        context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
         context.Response.Headers.Append("X-Frame-Options", "DENY");
         context.Response.Headers.Append("X-XSS-Protection", "1; mode=block");
         context.Response.Headers.Append("Referrer-Policy", "strict-origin-when-cross-origin");
